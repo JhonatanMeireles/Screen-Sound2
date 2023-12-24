d68da1ca90d6e2765f3ee7f4f4cf36ef890be77b
Serpa <jhonatanmeireles12@yahoo.com.br>
2023-12-24 19:47:46 -0300

    Incluindo validação  no construtor Avaliacao

--- ScreenSound/Modelos/Avaliacao.cs
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -4,8 +4,26 @@ internal class Avaliacao
 {
     public int Nota {  get; }
     public Avaliacao(int nota) 
-    { 
-        Nota = nota;
+    {
+
+        if (nota >= 0 && nota <= 10)
+        {
+            Nota = nota;
+        }
+        else
+        {
+            if (nota < 0)
+            {
+                Console.WriteLine("O número digitado é menor que 0, portanto será alterado para 0.");
+                Nota = 0; 
+            }
+            else if (nota > 10)
+            {
+                Console.WriteLine("Número digitado maior que 10. Será considerada a nota 10");
+                Nota = 10;
+            }
+        }
+            
     }
 
     public bool NotaValida() 
