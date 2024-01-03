970d7fb482c557df5f5a62718e056f23c737f761
Serpa <jhonatanmeireles12@yahoo.com.br>
2024-01-02 23:42:11 -0300

    Praticando a criação de uma calculadora

---
+++ Exercicio4_Calculadora/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Exercicio4_Calculadora.OperacoesMatematica;
+
+Console.WriteLine("Informe o primeiro número:");
+double number1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo número:");
+double number2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Escolha um dos operadores (+, -, *, /, ^, ou r) para o calculo:");
+char operador = char.Parse(Console.ReadLine());
+
+double v = Calculadora.Calcular(number1, number2, operador);
+Console.WriteLine(v);
